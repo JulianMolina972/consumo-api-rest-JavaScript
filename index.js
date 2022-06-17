3c6b602073f658f358fddd8987a582485d186ed8
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-06-17 18:28:49 -0500

    api key

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-const URL = 'https://api.thedogapi.com/v1/images/search';
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
 
 
 
@@ -14,14 +14,22 @@ const URL = 'https://api.thedogapi.com/v1/images/search';
 
 async function myDog() {
   try{
-    const res = await fetch(URL);
+    const res = await fetch(API_URL);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
   } catch (error){
     console.log('error');
   }
 }
+
+myDog();
 
 const myButton = document.querySelector('button');
 myButton.onclick = myDog;
