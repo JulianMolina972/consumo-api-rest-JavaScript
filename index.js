23cd5119781aa1357627d6f41048bb8b8795bac7
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-06-19 11:40:35 -0500

    GET AND POST, API REST

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
+
+const spanError = document.getElementById('error');
 
 
 
@@ -12,24 +15,71 @@ const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=174f
 //     img.src = data[0].url;  
 //   });
 
-async function myDog() {
+async function loadRandomMyDog() {
   try{
-    const res = await fetch(API_URL);
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
-
+    console.log('Random')
     console.log(data);
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    
+    if (res.status !== 200) {
+      spanError.innerHTML = 'There was an error' + res.status;
+    } else {
+
+      const img1 = document.getElementById('img1');
+      const img2 = document.getElementById('img2');
+      const img3 = document.getElementById('img3');
+      img1.src = data[0].url;
+      img2.src = data[1].url;
+      img3.src = data[2].url;
+    }
+    
+
   } catch (error){
     console.log('error');
   }
 }
 
-myDog();
+async function loadFavoritesMyDog() {
+  try{
+    const res = await fetch(API_URL_FAVORITES);
+    const data = await res.json();
+    console.log('Favorites')
+    console.log(data);
+
+    if (res.status !== 200) {
+      spanError.innerHTML = 'There was an error' + res.status + data.message;
+    } 
+    
+  } catch (error){
+    console.log('error');
+  }
+}
+
+async function saveFavoritesMyDog() {
+  const res = await fetch(API_URL_FAVORITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: 'gJA_mt09Q',
+    }),
+  });
+
+  const data = await res.json();
+  console.log('save');
+  console.log(res);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = 'There was an error' + res.status + data.message;
+  } 
+}
+
+
+loadRandomMyDog();
+loadFavoritesMyDog();
+
 
 const myButton = document.querySelector('button');
-myButton.onclick = myDog;
+myButton.onclick = loadRandomMyDog;
