99718ab9199c2edb578dfe6664067b0c1136eafe
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-06-25 14:11:48 -0500

    POST AND DELETE API dogs

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 
-const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
-const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?limit=3&api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
+const API_URL_RANDOM = 'https://api.thedogapi.com/v1/images/search?limit=3';
+const API_URL_FAVORITES = 'https://api.thedogapi.com/v1/favourites?api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79';
+const API_URL_FAVORITES_DELETE = (id) => `https://api.thedogapi.com/v1/favourites/${id}?api_key=174f7082-a54c-4bd1-8501-2bd86fe0de79`;
 
 const spanError = document.getElementById('error');
 
@@ -29,10 +30,20 @@ async function loadRandomMyDog() {
       const img1 = document.getElementById('img1');
       const img2 = document.getElementById('img2');
       const img3 = document.getElementById('img3');
+      const btn1 = document.getElementById('btn1');
+      const btn2 = document.getElementById('btn2');
+      const btn3 = document.getElementById('btn3');
+
       img1.src = data[0].url;
       img2.src = data[1].url;
       img3.src = data[2].url;
+
+      btn1.onclick = () => saveFavoriteMyDog(data[0].id);
+      btn2.onclick = () => saveFavoriteMyDog(data[1].id);
+      btn3.onclick = () => saveFavoriteMyDog(data[2].id);
+
     }
+
     
 
   } catch (error){
@@ -49,21 +60,45 @@ async function loadFavoritesMyDog() {
 
     if (res.status !== 200) {
       spanError.innerHTML = 'There was an error' + res.status + data.message;
-    } 
+    } else {
+      const section = document.getElementById('favoritesDogs');
+      section.innerHTML = "";
+      const h2 = document.createElement('h2');
+      const h2Text = document.createTextNode('Favorites Dogs');
+      h2.appendChild(h2Text);
+      section.appendChild(h2);
+      data.forEach(dog => {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        const btn = document.createElement('button');
+        const btnText = document.createTextNode('take out dog in favorites');
+
+        img.src = dog.image.url;
+        img.width = 250;
+        btn.appendChild(btnText);
+        btn.onclick = () => deleteFavoritesMyDog(dog.id);
+
+        article.appendChild(img);
+        article.appendChild(btn);
+        section.appendChild(article);
+
+        
+      });
+    }
     
   } catch (error){
     console.log('error');
   }
 }
 
-async function saveFavoritesMyDog() {
+async function saveFavoriteMyDog(id) {
   const res = await fetch(API_URL_FAVORITES, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      image_id: 'gJA_mt09Q',
+      image_id: id,
     }),
   });
 
@@ -73,7 +108,27 @@ async function saveFavoritesMyDog() {
 
   if (res.status !== 200) {
     spanError.innerHTML = 'There was an error' + res.status + data.message;
-  } 
+  } else {
+    console.log('Dog saved in favorites');
+    loadFavoritesMyDog();
+  }
+}
+
+async function deleteFavoritesMyDog(id) {
+  const res = await fetch(API_URL_FAVORITES_DELETE(id), {
+    method: 'DELETE',
+  });
+
+  const data = await res.json();
+  console.log('Delete');
+  console.log(res);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = 'There was an error' + res.status + data.message;
+  } else {
+    console.log('Dog removed from favorites');
+    loadFavoritesMyDog();
+  }
 }
 
 
@@ -81,5 +136,3 @@ loadRandomMyDog();
 loadFavoritesMyDog();
 
 
-const myButton = document.querySelector('button');
-myButton.onclick = loadRandomMyDog;
